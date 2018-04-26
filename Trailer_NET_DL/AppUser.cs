5bfa0036bfd47736b13a6d3ad754a03efb0b15ba
Prince Olelewe <GD0006@fidelitybank.ng>
2018-04-26 16:57:03 +0100

    Removed migration and made some changes

--- Trailer_NET_DL/AppUser.cs
+++ Trailer_NET_DL/AppUser.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using Trailer_NET_Library.Entities;
 
 namespace Trailer_NET_DL
@@ -8,5 +11,13 @@ namespace Trailer_NET_DL
     {
         public virtual ICollection<Movie> Liked { get; set; }
         public virtual ICollection<Movie> Watch_Later { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
