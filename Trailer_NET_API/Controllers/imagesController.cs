ed05ffce8c1b334e537a3413850eaa9159c870bc
Prince Olelewe <GD0006@fidelitybank.ng>
2018-04-26 17:51:44 +0100

    Added image controller

---
+++ Trailer_NET_API/Controllers/imagesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Trailer_NET_DL.Concrete;
+using Trailer_NET_Library.Entities;
+
+namespace Trailer_NET_API.Controllers
+{
+    [RoutePrefix("api/images")]
+    public class imagesController : ApiController
+    {
+        AppDbContext _db = new AppDbContext();
+
+        public async Task<IEnumerable<Image>> get(int id)
+        {
+            var query = "SELECT * FROM Image WHERE MovieID = @p0";
+            return await _db.Image.SqlQuery(query, id).ToListAsync();
+        }
+    }
+}
