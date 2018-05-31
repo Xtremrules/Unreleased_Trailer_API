26e55e1bc498e999d91bf44f8d435f5b5b7b0f9c
Xtremrules <oleleweprince@outlook.com>
2018-05-31 23:14:59 +0100

    added a package

--- Trailer_NET_API/Global.asax.cs
+++ Trailer_NET_API/Global.asax.cs
@@ -13,6 +13,20 @@ namespace Trailer_NET_API
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             GlobalConfiguration.Configuration.Filters.Add(new ValidateViewModelAttribute());
+
+            string youtubeEmbed = System.Configuration.ConfigurationManager.AppSettings["youtubeEmbed"];
+            string youtubeImg = System.Configuration.ConfigurationManager.AppSettings["youtubeImg"];
+
+            AutoMapper.Mapper.Initialize(x =>
+            {
+                x.CreateMap<Models.Movie, Trailer_NET_Library.Entities.Movie>()
+                .ForSourceMember(w => w.Image_Url, c => c.Ignore())
+                .ForMember(w => w.Youtube_Video_Id, c => c.MapFrom(y => y.Trailer_Url));
+
+                x.CreateMap<Trailer_NET_Library.Entities.Movie, Models.Movie>()
+                .ForMember(w => w.Trailer_Url, c => c.MapFrom(y => string.Format(youtubeEmbed, y.Youtube_Video_Id)))
+                .ForMember(w => w.Image_Url, c => c.MapFrom(y => string.Format(youtubeImg, y.Youtube_Video_Id)));
+            });
         }
     }
 }
